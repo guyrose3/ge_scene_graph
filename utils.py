395ac81f48db1363fdefe2eeb1a9ee07cf0fe05e
guyrose3 <guyrose3@gmail.com>
2016-05-04 08:10:53 +0300

    add directory for scene graph generator

--- utils.py
+++ utils.py
@@ -4,24 +4,10 @@ paths = imp.load_source('', os.path.join(os.path.dirname(__file__),'init_paths.p
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-import dlib
-import cv2
-from detector.obj_detector import detector
 
 ###########################
 #helper functions
 ###########################
-def get_obj_proposals(image_path):
-	img = cv2.imread(image_path)
-	rects = []
-	dlib.find_candidate_object_locations(img, rects, min_size=500,kvals=(100,100,1))
-	#dlib.find_candidate_object_locations(img, rects, min_size=500), default activation
-	#convert to fast-rcnn format
-	boxes = np.zeros((0,4),dtype=np.float)
-	for r in rects:
-		boxes = np.vstack((boxes,np.array([r.left(),r.top(),r.right(),r.bottom()],dtype=np.float)))
-	return boxes
-
 def convert_obj_data(data,obj_labels_file):
     with open(obj_labels_file) as f: obj_names = f.readlines()
     obj_names = ['__background__'] + [o.strip() for o in obj_names]
@@ -37,96 +23,6 @@ def convert_obj_data(data,obj_labels_file):
             image_reg['objects'].append(obj_reg)
         data_out.append(image_reg)
     return data_out
-
-
-def calc_bbs_features(bb1,bb2):
-    bb1 = bb1.astype(np.float)
-    bb2 = bb2.astype(np.float)
-    f1 = (bb1[0]-bb2[0])/(bb1[2]-bb1[0])
-    f2 = (bb1[1]-bb2[1])/(bb1[3]-bb1[1])
-    f3 = (bb1[2]-bb1[0])/(bb2[2]-bb2[0])
-    f4 = (bb1[3]-bb1[1])/(bb2[3]-bb2[1])
-    return np.array([f1,f2,f3,f4])
-    
-def calc_bb_probability(model,bb1,bb2):
-    #calc features
-    if 'reverse' in model.keys() and model['reverse']: f = calc_bbs_features(bb2,bb1)
-    else                                             : f = calc_bbs_features(bb1,bb2)
-    #apply gmm
-    dim = f.shape[0]
-    density = 0.0
-    for i in range(model['weights'].shape[0]):
-        mu = model['means'][i]
-        w  = model['weights'][i]
-        if len(model['covars'][i].shape)==2:
-            covar = model['covars'][i]
-        else:
-            covar = np.diag(model['covars'][i])
-        f_c = f-mu
-        inv_cov = np.linalg.inv(covar)
-        nom = np.exp(-0.5 * np.dot(f_c.T , np.dot(inv_cov , f_c)))
-        denom = np.sqrt(((2*np.pi)**dim) * np.linalg.det(covar))
-        density = density + (w*(nom/denom))
-    #apply sigmoid to obtain probability
-    return 1. / (1. + np.exp(density * model['sigmoid'][0] + model['sigmoid'][1]))
-
-###########################
-#relationship runner module
-###########################
-class rel_runner(object):
-    def __init__(self,rel_models,rel_prior=None,top_k=3):
-        self.rel_models = rel_models
-        self.obj_list   = []
-        self.rel_prior = rel_prior
-        self.rel_list = []
-        self.top_k = top_k
-
-    def set_image_objects(self,obj_list):
-        self.obj_list = obj_list
-
-    def get_rel_list(self):
-        rel_list = self.rel_list
-        self.rel_list = []
-        return rel_list
-
-    def run(self):
-        '''iterate all object pairs with all possible relationships'''
-        for idx1 in range(len(self.obj_list)):
-            obj1 = self.obj_list[idx1]
-            for idx2 in range(idx1+1,len(self.obj_list)):
-                obj2 = self.obj_list[idx2]
-                pair_rels = self._run_pair(obj1,obj2)
-                self.rel_list.append({'o1':idx1,'o2':idx2,'rels':pair_rels})
-
-
-    def _run_pair(self,obj1,obj2):
-        rel_probs = {'o1_o2' : np.zeros(len(self.rel_models)),'o2_o1' :np.zeros(len(self.rel_models))}
-        for idx,rel_models in enumerate(self.rel_models):
-            model = self.get_model(obj1['object'],obj2['object'],rel_models)
-            rel_probs['o1_o2'][idx] = calc_bb_probability(model,obj1['bbox'],obj2['bbox'])
-            model = self.get_model(obj2['object'],obj1['object'],rel_models)
-            rel_probs['o2_o1'][idx] = calc_bb_probability(model,obj2['bbox'],obj1['bbox'])
-        #keep top k for each side
-        top_k_1 = np.argsort(-rel_probs['o1_o2'])[:self.top_k]
-        rel_probs['o1_o2'] = np.vstack((top_k_1,rel_probs['o1_o2'][top_k_1]))
-        top_k_2 = np.argsort(-rel_probs['o2_o1'])[:self.top_k]
-        rel_probs['o2_o1'] = np.vstack((top_k_2,rel_probs['o2_o1'][top_k_2]))
-
-        return rel_probs
-
-    def get_model(self,obj1_idx,obj2_idx,rel_models):
-        selected_sub_model = rel_models[0]#object agnostic model
-        for sub_model in rel_models:
-            if tuple(sub_model['objects'])==(obj1_idx,obj2_idx):
-                selected_sub_model = sub_model
-                break
-        #add relationship to tables
-        model = {'weights'  : selected_sub_model['weights'],
-                 'means'    : selected_sub_model['means'],
-                 'covars'   : selected_sub_model['covars'],
-                 'sigmoid'  : selected_sub_model['sigmoid']}         
-
-        return model
 
 ###########################
 #Scene viewer module
@@ -233,52 +129,6 @@ class scene_viewer(object):
 		#plt.show()
 		plt.close()	
     
-###############################
-#object,attribute runner module
-###############################
-import caffe
-class sg_caffe_runner(object):
-	def __init__(self,object_path,attribute_path,gpu_mode):
-		self.gpu_mode = gpu_mode
-		self.object_det = {}
-		self.attribute_det = {}
-		self.object_det['path'] = object_path
-		self.attribute_det['path'] = attribute_path
-		self.load_caffe_nets()
-
-	def load_caffe_nets(self):
-		#pretrained imagenet weights
-		caffemodel = os.path.join(paths.FAST_RCNN_BASE,'data/imagenet_models/CaffeNet.v2.caffemodel')
-		
-		#load object
-		obj_class_path = os.path.join(self.object_det['path'],'labels.txt')
-		obj_prototxt   = os.path.join(self.object_det['path'],'test.prototxt')
-		svm_path_obj   = os.path.join(self.object_det['path'],'train_output/train.default')
-		self.object_det['detector'] = detector(obj_prototxt,caffemodel,obj_class_path,gpu_mode=self.gpu_mode,\
-											   use_svm=True,output_all_boxes=True,svm_path=svm_path_obj)
-		#load attribute
-		att_class_path = os.path.join(self.attribute_det['path'],'labels.txt')
-		att_prototxt   = os.path.join(self.attribute_det['path'],'test.prototxt')
-		svm_path_att   = os.path.join(self.attribute_det['path'],'train_output/train.default')
-		self.attribute_det['detector'] = detector(att_prototxt,caffemodel,att_class_path,gpu_mode=self.gpu_mode,\
-											      use_svm=True,output_all_boxes=True,svm_path=svm_path_att)
-	
-	def get_object_detections(self,image_path):
-		self.run_detection(self.object_det['detector'],image_path)
-		return self.object_det['detector'].detections
-
-
-	def get_attribute_detections(self,image_path):
-		self.run_detection(self.attribute_det['detector'],image_path)
-		return self.attribute_det['detector'].detections
-
-
-	def run_detection(self,detector,image_path):
-		#generate object proposals
-		boxes = get_obj_proposals(image_path)
-		#run detector
-		detector.detect_single_aux(image_path,boxes)
-
 #example usage
 if __name__ == '__main__':
 
