d54b65672510f3ee4281bb37a067862ec1e95916
guyrose3 <guyrose3@gmail.com>
2016-06-13 11:05:14 +0300

    adding the rest of the file

--- utils.py
+++ utils.py
@@ -8,6 +8,20 @@ import matplotlib.pyplot as plt
 ###########################
 #helper functions
 ###########################
+def bbox_IoU(bb1,bb2):
+    #calc bb overlap
+    bi=[max(bb1[0],bb2[0]) , max(bb1[1],bb2[1]) , min(bb1[2],bb2[2]) , min(bb1[3],bb2[3])]
+    iw=bi[2]-bi[0]+1
+    ih=bi[3]-bi[1]+1
+    if iw>0 and ih>0:
+        #compute overlap as area of intersection / area of union
+        ua=(bb1[2]-bb1[0]+1)*(bb1[3]-bb1[1]+1)+ \
+           (bb2 [2]-bb2 [0]+1)*(bb2 [3]-bb2 [1]+1)- \
+            iw*ih
+        ov=iw*ih/ua
+        return ov
+    return 0.
+
 def convert_obj_data(data,obj_labels_file):
     with open(obj_labels_file) as f: obj_names = f.readlines()
     obj_names = ['__background__'] + [o.strip() for o in obj_names]
